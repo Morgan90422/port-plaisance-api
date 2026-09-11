a9e527de24c66d2a2a4d2532c039e53b99174f57
Morgan <morgan.ancelin@hotmail.fr>
2026-09-11 10:15:45 +0200

    ajout url render à swagger.js

--- config/swagger.js
+++ config/swagger.js
@@ -9,7 +9,10 @@ const options = {
       description:
         "API de gestion des catways et des réservations pour la capitainerie du port de plaisance Russell.",
     },
-    servers: [{ url: 'http://localhost:3000', description: 'Serveur local' }],
+    servers: [
+  { url: 'http://localhost:3000', description: 'Serveur local' },
+  { url: 'https://port-plaisance-api-62wb.onrender.com', description: 'Serveur de production' },
+],
     components: {
       securitySchemes: {
         cookieAuth: {
